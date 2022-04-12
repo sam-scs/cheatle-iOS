e03289d8a393dd2275b62911ecb7cb96bd26645f
Sam Johnson <sam@howldesign.com>
2022-04-11 22:06:57 -0500

    Initial Commit

---
+++ cheatle/cheatleApp.swift
@@ -0,0 +1,17 @@
+//
+//  cheatleApp.swift
+//  cheatle
+//
+//  Created by Sam Johnson on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct cheatleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
